0861faba597eec009f12d2ce37ee0e8dafa541b7
Brian Takita <brian.takita@gmail.com>
2021-12-08 12:41:43 -0600

    ## 12.1.0
    ### Minor Changes
    
    - svelte-kit integration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ctx-core/auth0-ui
 
+## 12.1.0
+
+### Minor Changes
+
+- svelte-kit integration
+
 ## 12.0.1
 
 ### Patch Changes
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export * from './auth0_ui_Ctx.js'
-export * from './auth0_ui_ctx_key.js'
-export * from './auth_script_html_.js'
-export * from './getContext_auth0_ui_ctx.js'
-export * from './ui/index.js'
+export * from './lib/index.js'
